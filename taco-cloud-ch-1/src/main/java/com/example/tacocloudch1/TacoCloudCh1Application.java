bfd116ca4b74e555234dc28587a3821fd81ad108
kusakina0608 <platina.kusakina@gmail.com>
2022-07-17 14:49:17 +0900

    Chapter 1- 프로젝트 생성 (｡•̀ᴗ-)✧

---
+++ taco-cloud-ch-1/src/main/java/com/example/tacocloudch1/TacoCloudCh1Application.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudch1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TacoCloudCh1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TacoCloudCh1Application.class, args);
+    }
+
+}
---
+++ taco-cloud-ch-1/src/test/java/com/example/tacocloudch1/TacoCloudCh1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tacocloudch1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TacoCloudCh1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
